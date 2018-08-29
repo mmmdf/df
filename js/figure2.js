b0e513624de41d7b543a9d002fe1d09db7a8f566
Thais Santos <thais@4bis.nl>
2018-08-29 14:39:58 +0200

    adding initial files

---
+++ js/figure2.js
@@ -0,0 +1,74 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "shapes": [], 
+        "autosize": true, 
+        "hiddenlabels": [
+            "CHR"
+        ], 
+        "hovermode": "closest", 
+        "breakpoints": []
+    }, 
+    "data": [
+        {
+            "sort": true, 
+            "labelssrc": "tahtah:3:e28a3e", 
+            "textfont": {
+                "color": "rgb(255, 255, 255)"
+            }, 
+            "autobinx": true, 
+            "name": "B", 
+            "labels": [
+                "DriveFly", 
+                "FHR", 
+                "APH", 
+                "SKYP", 
+                "L4P", 
+                "NEW", 
+                "UKPF", 
+                "P and G", 
+                "CHR", 
+                "EX", 
+                "SPF", 
+                "TT", 
+                "ILMAG"
+            ], 
+            "direction": "counterclockwise", 
+            "values": [
+                "401", 
+                "128", 
+                "107", 
+                "1036", 
+                "401", 
+                "2", 
+                "43", 
+                "6", 
+                "1232", 
+                "2542", 
+                "130", 
+                "5", 
+                "103"
+            ], 
+            "mode": "markers", 
+            "hoverinfo": "label+value+text+percent", 
+            "marker": {
+                "line": {
+                    "color": "rgb(255, 255, 255)", 
+                    "width": 1
+                }, 
+                "colors": [
+                    "rgb(190, 207, 182)", 
+                    "rgb(125, 178, 143)", 
+                    "rgb(40, 144, 126)", 
+                    "rgb(16, 125, 121)", 
+                    "rgb(24, 97, 108)", 
+                    "rgb(28, 71, 93)"
+                ]
+            }, 
+            "textinfo": "label", 
+            "type": "pie", 
+            "valuessrc": "tahtah:3:d3849a", 
+            "autobiny": true
+        }
+    ]
+}
---
+++ js/figure3.js
@@ -0,0 +1,85 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "yaxis": {
+            "range": [
+                0, 
+                92.27368421052631
+            ], 
+            "type": "linear", 
+            "autorange": true, 
+            "title": "Consolidators"
+        }, 
+        "showlegend": false, 
+        "breakpoints": [], 
+        "xaxis": {
+            "range": [
+                -0.5, 
+                12.5
+            ], 
+            "type": "category", 
+            "autorange": true, 
+            "title": "Per Booking"
+        }, 
+        "hovermode": "closest"
+    }, 
+    "data": [
+        {
+            "autobinx": true, 
+            "name": "B", 
+            "orientation": "v", 
+            "ysrc": "tahtah:5:084586", 
+            "textsrc": "tahtah:5:084586", 
+            "mode": "markers", 
+            "xsrc": "tahtah:5:ca5a34", 
+            "text": [
+                "0", 
+                "0", 
+                "26.08", 
+                "62.58", 
+                "63.15", 
+                "64.8", 
+                "68.59", 
+                "70.73", 
+                "72.13", 
+                "78.07", 
+                "80.92", 
+                "86.64", 
+                "87.66"
+            ], 
+            "y": [
+                "0", 
+                "0", 
+                "26.08", 
+                "62.58", 
+                "63.15", 
+                "64.8", 
+                "68.59", 
+                "70.73", 
+                "72.13", 
+                "78.07", 
+                "80.92", 
+                "86.64", 
+                "87.66"
+            ], 
+            "x": [
+                "P and G", 
+                "NEW", 
+                "ILMAG", 
+                "SPF", 
+                "CHR", 
+                "UKPF", 
+                "EX", 
+                "SKYP", 
+                "FHR", 
+                "L4P", 
+                "TT", 
+                "DriveFly", 
+                "APH"
+            ], 
+            "type": "bar", 
+            "autobiny": true
+        }
+    ]
+}
